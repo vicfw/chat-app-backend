638df9e6a74747ecf29b94cadb88aa4263db75cf
Farid <61201661+vicfw@users.noreply.github.com>
2022-11-14 12:19:07 +0330

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ server.listen(process.env.PORT, function () {});
 
 const io = new Server(server,{
    cors: {
-    origin: "https://example.com",
+    origin: proccess.env.CLIENT_URI,
     credentials: true
   });
 
