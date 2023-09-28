9e02d25f42f0de43a3c489120ea395a7f2c6e114
Farid <faridforward222@gmail.com>
2023-09-28 09:48:09 +0330

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,53 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/userRoutes';
-import messageRoute from './routes/messageRoute';
-import { Server } from 'socket.io';
-import http from 'http';
+import express, { Application } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoutes";
+import messageRoute from "./routes/messageRoute";
+import { Server } from "socket.io";
+import http from "http";
 
 const app: Application = express();
 
 const server = http.createServer(app);
 
 dotenv.config();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.use('/api/auth', userRouter);
-app.use('/api/messages', messageRoute);
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRoute);
 
 mongoose.connect(process.env.MONGODB_URI!, () => {
-  console.log('mongo db connected');
+  console.log("mongo db connected");
 });
 
 server.listen(process.env.PORT, function () {});
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 // @ts-ignore
 global.onlineUsers = new Map();
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  console.log("connected");
+
   // @ts-ignore
   global.chatSocket = socket;
-  socket.on('add-user', (userId) => {
+  socket.on("add-user", (userId) => {
     // @ts-ignore
     onlineUsers.set(userId, socket.id);
   });
 
-  socket.on('send-msg', (data) => {
+  socket.on("send-msg", (data) => {
     // @ts-ignore
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit('msg-recieve', data.msg);
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
     }
   });
+  console.log(global);
 });
